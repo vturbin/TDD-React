fb1c85c7663521dcfcb6ffb122bda71ef1ede882
vturbin <v.turbin@hotmail.com>
2020-12-27 13:49:07 +0100

    refactor test

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -5,22 +5,34 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />)
+
+const findByTestAttr = (wrapper, val) => {
+  const selector = `[data-test="${val}"]`;
+  return wrapper.find(selector);
+} 
+
 test("renders without error", () => {
-  const wrapper = shallow(<App/>)
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper,"component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test("renders button", () => {
-  const wrapper = shallow(<App/>)
-  const button = wrapper.find("[data-test='increment-button']")
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper,"increment-button");
   expect(button.length).toBe(1)
 });
 
 
 test("renders counter display", () => {
-  const wrapper = shallow(<App/>)
-  const display = wrapper.find("[data-test='counter-display']")
+  const wrapper = setup();
+  const display = findByTestAttr(wrapper,"counter-display");
   expect(display.length).toBe(1)
 });
 
