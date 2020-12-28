5895d5c73b1501f778680dd9a09ba76603d68ccb
vturbin <v.turbin@hotmail.com>
2020-12-28 12:40:36 +0100

    code useEffect and useReducer to pass test

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -2,17 +2,36 @@ import "./App.css";
 import React, { Component } from "react";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
+import hookActions from "./actions/hookActions";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container" data-test="component-app">
-      <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[{guessedWord:"roller", letterMatchCount:3}]} />
-      </div>
-    );
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
   }
+}
+
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  return (
+    <div className="container" data-test="component-app">
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: "roller", letterMatchCount: 3 }]}
+      />
+    </div>
+  );
 }
 
 export default App;
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,10 +1,19 @@
 import App from './App';
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 import {findByTestAttr} from "../test/testUtils";
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+import hookActions from "./actions/hookActions";
+
+const mockGetSecretWord = jest.fn();
+
+
 const setup = () => {
-    return shallow(<App/>)
+    mockGetSecretWord.mockClear();
+    hookActions.getSecretWord = mockGetSecretWord;
+
+    // Use mount, because useEffect not called on 'shallow'
+    return mount(<App/>)
 }
 
 test('renders without error', () => {
@@ -12,3 +21,13 @@ test('renders without error', () => {
     const component = findByTestAttr(wrapper,'component-app')
     expect(component.length).toBe(1);
 });
+
+
+describe('getSecretWord calls',()=> {
+    test('getSecretWord gets called on App mount', ()=> {
+        setup();
+
+        // check to see if secret word was updated
+        expect(mockGetSecretWord).toHaveBeenCalled();
+    })
+})
